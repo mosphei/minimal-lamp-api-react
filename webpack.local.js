4be321302275e8087e411c0eeb3d41a9ff7ef726
jarom <jb22@packetsmiter.com>
2018-09-01 10:36:16 -0700

    added webpack dev server added local config

---
+++ webpack.local.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    "app.js":'./src/index.js'
+  },
+  output: {
+    filename: '[name]',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+      contentBase: './dist'
+  },
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        },
+        {
+            test: /\.php$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                    }
+                }
+            ]
+        }
+      ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+        template:'src/index.html'
+    }),
+    new webpack.NormalModuleReplacementPlugin(/service/, function (resource) {
+        resource.request = resource.request.replace(/service.js/, `mock.js`);
+    })
+  ]
+};
